188f7d6e54aa001ec09795a06881d8d74896843b
acastrofarias <acastrofarias13@gmail.com>
2023-03-21 18:41:49 -0700

    checkpoint 3

--- cryptohustlelite/src/App.jsx
+++ cryptohustlelite/src/App.jsx
@@ -6,6 +6,25 @@ const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function App() {
   const [list, setList] = useState(null);
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
+  const searchItems = searchValue => {
+    setSearchInput(searchValue);
+    if (searchValue !== "") {
+      const filteredData = Object.keys(list.Data).filter((item) => 
+        Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchValue.toLowerCase())
+      )
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(Object.keys(list.Data));
+    }
+
+    
+  };
   
   const fetchAllCoinData = async () => {
     const response = await fetch( 
@@ -23,17 +42,35 @@ function App() {
   return (
     <div className="whole-page">
       <h1>My Crypto List</h1>
-          <ul>
-          {list && Object.entries(list.Data).map(([coin]) =>
-            list.Data[coin].PlatformType === "blockchain" ? ( 
-              <CoinInfo
-                image={list.Data[coin].ImageUrl}
-                name={list.Data[coin].FullName}
-                symbol={list.Data[coin].Symbol}
-                />
-              ) : null
-          )}  
-          </ul>
+
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(inputString) => searchItems(inputString.target.value)}
+      />
+
+      <ul>
+      {searchInput.length > 0
+      ? filteredResults.map((coin) => 
+            list.Data[coin].PlatformType === "blockchain" ? 
+            <CoinInfo
+              image={list.Data[coin].ImageUrl}
+              name={list.Data[coin].FullName}
+              symbol={list.Data[coin].Symbol}
+            />
+            : null
+        )
+      : list && Object.entries(list.Data).map(([coin]) => 
+            list.Data[coin].PlatformType === "blockchain" ? 
+            <CoinInfo
+              image={list.Data[coin].ImageUrl}
+              name={list.Data[coin].FullName}
+              symbol={list.Data[coin].Symbol}
+            />
+            : null
+        )}
+      </ul>
+
     </div>
   )
 }
